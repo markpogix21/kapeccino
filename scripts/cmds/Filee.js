72da4786cdc85cd725702ad6c770ea450b9b9c8a
markpogix21 <luckygithub387@gmail.com>
2024-08-16 11:59:42 +0800

    Create Filee.js

---
+++ scripts/cmds/Filee.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+module.exports = {
+  config: {
+    name: "sendfile",
+    aliases: ["file"],
+    version: "1.0",
+    author: "Mahir Tahsan",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "Tools 🛠",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["61563419107727",];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("You don't have permission to use this command. 😆", event.threadID, event.messageID);
+    }
+
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+    }
+
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
+};
