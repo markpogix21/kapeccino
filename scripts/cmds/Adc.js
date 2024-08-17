d1fdc8fa0045eef640e090701b6563a3d0d2919c
markpogix21 <luckygithub387@gmail.com>
2024-08-17 08:53:38 +0800

    Update Adc.js

--- scripts/cmds/Adc.js
+++ scripts/cmds/Adc.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	
 onStart: async function({ api, event, args }) {
-  const permission = ["100095290150085"];
+  const permission = ["61563419107727"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("❌ | You aren't allowed to use this command.", event.threadID, event.messageID);
     const axios = require('axios');
